8bd1c1041b7538b1ea1b0c1f47ebaeaeb775adb2
tkon3 <tkoncrypto3@gmail.com>
2021-02-22 14:16:10 +0100

    add loss curve

--- plot.py
+++ plot.py
@@ -14,6 +14,7 @@ def make_dirs():
     os.makedirs(saving_path)    
     os.makedirs(saving_path + "/top")
     os.makedirs(saving_path + "/segmented")
+    os.makedirs(saving_path + "/loss")
     return saving_path
 
 def get_config(file):
@@ -49,7 +50,6 @@ def get_top(shapley, top):
     _, idx = torch.topk(shapley, k=top, dim=-1)
     mask = mask.scatter(dim=-1, index=idx, src=torch.ones_like(idx).float())
     return mask.reshape(1, 224, 224)
-
 
 def filter_imgs(base, shapleys, top, n_samples, segmentation=None):
     outputs = []
@@ -91,7 +91,31 @@ def plot(base, processed, filtered_imgs, names, n_samples, path):
                 ax.axis("off")
         
         plt.savefig(path + "/" + str(n))
+        plt.close()
 
+def get_curves(paths):
+    idx, ab, line = [], [], []
+    for i, path in enumerate(paths):
+        data = np.load(path + "/shapley/loss.npy")
+        ab.append(data[:,1])
+        line.append(data[:,3])
+    return data[:,0], np.array(ab).T, np.array(line).T
+
+def plot_lines(idx, ab, line, saving_path, names):
+    fig = plt.figure(figsize=(8, 6))
+    plt.plot(idx, ab)
+    plt.legend(names, loc="best")
+    plt.ylabel("Avg. abs. prob. difference")
+    plt.xlabel("Top % pixels kept")
+    plt.savefig(saving_path + "/loss/abs")
+
+    fig = plt.figure(figsize=(8, 6))
+    plt.plot(idx, line)
+    plt.legend(names, loc="best")
+    plt.ylabel("Avg. prob. difference")
+    plt.xlabel("Top % pixels kept")
+    plt.savefig(saving_path + "/loss/avg")
+    
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--plot_config", help="path to the trainer config", type=str, default="config/plot_config/plot_config.json")
@@ -106,8 +130,15 @@ names = get_names(paths)
 shapleys = get_shapley(paths)
 saving_path = make_dirs()
 
+#IMGS
 filtered_imgs = filter_imgs(base, shapleys, config.top, config.n_samples)
 plot(base, processed, filtered_imgs, names, config.n_samples, saving_path + "/top")
 
 filtered_imgs = filter_imgs(base, shapleys, config.top, config.n_samples, segmentation)
 plot(base, processed, filtered_imgs, names, config.n_samples, saving_path + "/segmented")
+
+#CURVES
+idx, ab, line = get_curves(paths)
+plot_lines(idx, ab, line, saving_path, names)
+
+
